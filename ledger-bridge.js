7081759f12e85943715d56557a82ee64829e9257
Monte Lai <monte.lai@consensys.net>
2025-12-19 15:33:18 +0800

    fix: remove log

--- ledger-bridge.js
+++ ledger-bridge.js
@@ -223,7 +223,6 @@ export default class LedgerBridge {
       //   appName,
       //   version,
       // };
-      console.log('LEDGER:::GET APP AND NAME', this.transport);
       const response = await this.transport.send(
         0xe0, // CLA
         0x01, // INS (GET_APP_NAME)
